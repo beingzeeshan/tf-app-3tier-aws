3117d79d42c1796c1ec6539820e4018c89fd41d0
beingzeeshan <zeeshan_ahmad@aol.com>
2022-11-15 17:04:36 +0530

    added nested-object code

---
+++ nested-object.py
@@ -0,0 +1,21 @@
+objectt = { "a" : { "b" : { "c" : "d" } } }
+
+targetKey = "a"
+
+def getValue(objectt, targetKey):
+    if(type(objectt) == str or type(objectt) == int or type(objectt) == float):
+        print("Nested object is ended !!")
+        return
+    
+    keysList = list(objectt.keys())
+    
+    if len(keysList) > 0 :
+        for keys in keysList:
+            if keys == targetKey:
+                print(f"Key found {keys} and value is {objectt[targetKey]}")
+                return objectt[targetKey]   
+        return getValue(objectt[keysList[0]], targetKey)
+        
+    elif(len(keysList) == 0):
+        print("Keys are empty")
+getValue(objectt, targetKey)
