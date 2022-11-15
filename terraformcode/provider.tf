3704ba17a5d645cb5a6382c6d4ebc859725fecf2
Zeeshan Ahmad <zeeshan_ahmad@aol.com>
2022-11-15 15:12:51 +0530

    Add files via upload

---
+++ terraformcode/provider.tf
@@ -0,0 +1,21 @@
+# Provider Configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+terraform {
+  backend "s3" {
+    bucket = "newterraformstatesfiles"
+    key    = "tfstates/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
